db890a86df9cc48240bd8b91d1650c9d11883e8b
Matías Barrios <matiasbarrios@gmail.com>
2017-11-20 14:44:56 -0300

    Makgin basic admin GUIs

--- time_log_reporter/reports/admin.py
+++ time_log_reporter/reports/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Team
+from .models import Member
+from .models import Project
 
-# Register your models here.
+
+
+admin.site.register(Team)
+admin.site.register(Member)
+admin.site.register(Project)
--- time_log_reporter/reports/models.py
+++ time_log_reporter/reports/models.py
@@ -5,19 +5,25 @@ from django.contrib.auth.models import User
 
 class Team(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 
 class Member(models.Model):
     name = models.CharField(max_length=200)
     avatar = models.CharField(blank=True, max_length=200)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
     estimated_hours = models.IntegerField()
     members = models.ManyToManyField(Member, through='Membership')
+    def __str__(self):
+        return self.name
 
 
 class Membership(models.Model):
