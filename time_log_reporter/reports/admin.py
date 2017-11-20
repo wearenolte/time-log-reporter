3af20714ad5697a5692ab93d9f49cf4afcf79d9b
Matías Barrios <matiasbarrios@gmail.com>
2017-11-20 15:47:58 -0300

    Adding easily memberships to a project.

--- time_log_reporter/reports/admin.py
+++ time_log_reporter/reports/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Team
 from .models import Member
+from .models import Membership
 from .models import Project
+
+
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline]
 
 
 
 admin.site.register(Team)
 admin.site.register(Member)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
