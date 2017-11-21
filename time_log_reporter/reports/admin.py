7e50351b798280be96008d00a4f301f958b100f5
Matías Barrios <matiasbarrios@gmail.com>
2017-11-21 14:51:38 -0300

    Improving the listing and edition of the schema.

--- time_log_reporter/reports/admin.py
+++ time_log_reporter/reports/admin.py
@@ -8,22 +8,67 @@ from .models import Project
 
 
 
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ('name', 'team')
+class MembersInline(admin.TabularInline):
+    model = Member
+    extra = 0
+    fields = ['name']
 
-    
+
+class ProjectsInline(admin.TabularInline):
+    model = Project
+    extra = 0
+    fields = ['name', 'estimated_hours']
+
+
 class MembershipInline(admin.TabularInline):
     model = Membership
-    extra = 1
+    extra = 0
 
 
-class ProjectAdmin(admin.ModelAdmin):
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'administrator', 'members_amount', 'projects_amount')
+    inlines = [MembersInline, ProjectsInline]
+
+    def administrator(self, object):
+        return object.admin
+    administrator.short_description = 'Administrator'
+    
+    def members_amount(self, object):
+        return object.members.count()
+    members_amount.short_description = 'Members'
+    
+    def projects_amount(self, object):
+        return object.projects.count()
+    projects_amount.short_description = 'Projects'
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team_admin', 'memberships_amount')
     inlines = [MembershipInline]
-    list_display = ('name', 'estimated_hours')
+    
+    def team_admin(self, object):
+        return object.team.name + ' / ' + object.team.admin.username
+    team_admin.short_description = 'Team / Administrator'
+    
+    def memberships_amount(self, object):
+        return object.memberships.count()
+    memberships_amount.short_description = 'Projects in'
+
+    
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'estimated_hours', 'team_admin', 'members_amount')
+    inlines = [MembershipInline]
+    
+    def team_admin(self, object):
+        return object.team.name + ' / ' + object.team.admin.username
+    team_admin.short_description = 'Team / Administrator'
+
+    def members_amount(self, object):
+        return object.members.count()
+    members_amount.short_description = 'Members'
 
 
-
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Project, ProjectAdmin)
 
--- time_log_reporter/reports/models.py
+++ time_log_reporter/reports/models.py
@@ -13,7 +13,7 @@ class Team(models.Model):
 class Member(models.Model):
     name = models.CharField(max_length=200)
     avatar = models.CharField(blank=True, max_length=200)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
     def __str__(self):
         return self.name
 
@@ -21,13 +21,15 @@ class Member(models.Model):
 class Project(models.Model):
     name = models.CharField(max_length=200)
     estimated_hours = models.IntegerField()
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='projects')
     members = models.ManyToManyField(Member, through='Membership')
     def __str__(self):
         return self.name
 
 
 class Membership(models.Model):
-    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='memberships')
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     role = models.CharField(max_length=50)
+    def __str__(self):
+        return ''
